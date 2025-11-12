960811f6b7c9ddbdfa6dee2cd617381801d5d257
Ali Efruz Yıldırır <efruzyildirir@gmail.com>
2025-11-12 15:31:44 +0300

    Allow calling build and other commands without specifying the module. Will set the ModuleFile to `.`

--- ebuild/Commands/ModuleCreatingCommand.cs
+++ ebuild/Commands/ModuleCreatingCommand.cs
@@ -37,7 +37,7 @@ namespace ebuild.Commands
     public abstract class ModuleCreatingCommand : BaseCommand
     {
         [CommandParameter(0, Description = "the module file to build")]
-        public required string ModuleFile { get; init; }
+        public string ModuleFile { get; init; } = ".";
         [CommandOption("configuration", 'c', Description = "the build configuration to use")]
         public string Configuration { get; init; } = Config.Get().DefaultBuildConfiguration;
         [CommandOption("toolchain", Description = "the toolchain to use")]
